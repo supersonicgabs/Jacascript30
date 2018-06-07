d5206c79fecfb46bc20cff010e8f92a433bfd6e6
supersonicgabs <gabrielfonseca.coutinho@outlook.com>
2018-06-07 01:09:46 -0300

    primeiro vídeo do curso

---
+++ js/drum-kit-1.js
@@ -0,0 +1,6 @@
+window.addEventListener('keydown', function(e) {
+    const audio = document.querySelector(`audio[data-key="${e.KeyCode}"]`);    
+    console.log(audio);
+    if(!audio) return; //stop the function from running all together
+    audio.play();
+});
