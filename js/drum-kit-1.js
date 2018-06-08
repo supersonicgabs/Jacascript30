74205ab1a8a4bb0d3f7f0af421058289dc478231
supersonicgabs <gabrielfonseca.coutinho@outlook.com>
2018-06-07 23:45:31 -0300

    finalização do exercício

--- js/drum-kit-1.js
+++ js/drum-kit-1.js
@@ -1,6 +1,19 @@
-window.addEventListener('keydown', function(e) {
-    const audio = document.querySelector(`audio[data-key="${e.KeyCode}"]`);    
+function playSound(e)
+{
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     console.log(audio);
     if(!audio) return; //stop the function from running all together
+    audio.currentTime = 0; //rewind to the start
     audio.play();
-}); 
+    console.log(key);
+    key.classList.add('playing');
+}
+function removeTransition(e){
+    if (e.propertyName !== 'transform') return; // skip it if its not a transform
+    this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
